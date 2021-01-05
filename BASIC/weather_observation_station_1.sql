45782c9df322421198f706f37b66e69b0ed0e9a0
Rajesh <rajeshjaiswal878@gmail.com>
2021-01-04 20:02:09 -0800

    weather_observation_station_1

---
+++ BASIC/weather_observation_station_1.sql
@@ -0,0 +1,9 @@
+/* Weather Observation Station 1
+Query a list of CITY and STATE from the STATION table
+The STATION table is described as follows:
+Ans: */
+
+SELECT
+    CITY,
+    STATE
+FROM STATION;
