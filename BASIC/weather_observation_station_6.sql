afd6fdbbb29eba0653d6667604d98e1cb2fbf842
Rajesh <rajeshjaiswal878@gmail.com>
2021-01-05 19:22:58 -0800

    weather_observation_station_6

---
+++ BASIC/weather_observation_station_6.sql
@@ -0,0 +1,12 @@
+/* Weather Observation Station 6
+Query the list of CITY names starting with
+vowels (i.e., a, e, i, o, or u) from STATION.
+Your result cannot contain duplicates.
+Input Format
+The STATION table is described as follows:
+*/
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE lower(CITY)
+          REGEXP '^[aeiou]';
