6120d282f2d21241bfd23d966f303703b15ccba3
Rajesh <rajeshjaiswal878@gmail.com>
2021-01-08 21:21:27 -0800

    weather_observation_station_15

---
+++ BASIC/weather_observation_station_15.sql
@@ -0,0 +1,10 @@
+/*
+Weather Observation Station 15
+Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than . Round your answer to  decimal places.
+Input Format
+The STATION table is described as follows:
+Ans:
+*/
+SELECT ROUND(LONG_W, 4)
+FROM STATION
+WHERE LAT_N = (SELECT MAX(LAT_N) FROM STATION WHERE LAT_N < 137.2345)
