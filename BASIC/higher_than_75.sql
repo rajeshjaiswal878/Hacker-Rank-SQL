8dd0aa5f99b288d7bb77494bfa74700d7fa78531
Rajesh Ashok Jaiswal <rajeshjaiswal878@gmail.com>
2021-02-21 16:11:46 -0800

    Problem Higher Than 75 Solution

---
+++ BASIC/higher_than_75.sql
@@ -0,0 +1,19 @@
+/*
+Higher Than 75 Marks
+Query the Name of any student in *STUDENTS* who scored higher than  Marks.
+Order your output by the last three characters of each name.
+If two or more students both have names ending in the
+same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+*Input Format*
+The *STUDENTS* table is described as follows:
+The Name column only contains uppercase (A-Z) and lowercase (a-z) letters.
+
+Problem Link:
+*/
+
+# My Solution:
+SELECT NAME
+FROM STUDENTS
+WHERE MARKS > 75
+ORDER BY SUBSTR(NAME, -3) ASC, ID;
+
