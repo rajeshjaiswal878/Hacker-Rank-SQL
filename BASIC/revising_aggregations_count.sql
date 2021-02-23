35c37c0eaa6550aa9dae0eca500048e71b46d043
Rajesh Ashok Jaiswal <rajeshjaiswal878@gmail.com>
2021-02-22 20:22:01 -0800

    Problem Revising Aggregation Count Solution

---
+++ BASIC/revising_aggregations_count.sql
@@ -0,0 +1,12 @@
+/*
+Revising Aggregations - The Count Function
+Query a count of the number of cities in CITY having a Population larger than .
+Ans:
+
+Problem Link:
+*/
+
+# My Solution:
+SELECT COUNT(1) FROM CITY WHERE POPULATION>100000;
+
+
