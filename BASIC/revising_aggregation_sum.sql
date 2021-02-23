0065a24b0c62f6b1548edfc9c33b7eeb327b2885
Rajesh Ashok Jaiswal <rajeshjaiswal878@gmail.com>
2021-02-22 20:22:32 -0800

    Problem Revising Aggregation Sum Solution

---
+++ BASIC/revising_aggregation_sum.sql
@@ -0,0 +1,11 @@
+/*
+Revising Aggregations - The Sum Function
+Query the total population of all cities in CITY where District is *California*.
+ANS:
+
+Problem Link:
+*/
+
+# My Solution:
+SELECT SUM(POPULATION) FROM CITY WHERE DISTRICT='California';
+
