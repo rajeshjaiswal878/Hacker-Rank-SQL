13cfe4fbc77c1d2c486d50dbe18ea1c91f8f5f69
Rajesh Ashok Jaiswal <rajeshjaiswal878@gmail.com>
2021-02-23 20:59:51 -0800

    Problem Population Density Difference Solution

---
+++ BASIC/population_density_difference.sql
@@ -0,0 +1,10 @@
+/*
+Population Density Difference
+Query the difference between the maximum and minimum populations in *CITY*.
+Problem Link:
+*/
+
+# My Solution:
+SELECT MAX(POPULATION) - MIN(POPULATION)
+FROM CITY;
+
