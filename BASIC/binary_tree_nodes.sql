18dcfb5619b2da41c403555e9121e4d9dec19248
Rajesh Ashok Jaiswal <rajeshjaiswal878@gmail.com>
2021-02-24 19:44:25 -0800

    Problem Binary Tree Nodes Solution

---
+++ BASIC/binary_tree_nodes.sql
@@ -0,0 +1,24 @@
+/*
+Binary Tree Nodes
+You are given a table, BST, containing two columns: N and P, where N represents
+the value of a node in Binary Tree, and P is the parent of N.
+Write a query to find the node type of Binary Tree ordered by the value of the node.
+Output one of the following for each node:
+
+Root: If node is root node.
+Leaf: If node is leaf node.
+Inner: If node is neither root nor leaf node.
+
+Problem Link:
+*/
+
+# My Solution:
+
+SELECT N,
+       CASE
+           WHEN P IS NULL THEN 'Root'
+           WHEN (SELECT COUNT(P) FROM BST WHERE P = B.N) > 0 THEN 'Inner'
+           ELSE 'Leaf'
+           END
+FROM BST AS B
+ORDER BY N;
